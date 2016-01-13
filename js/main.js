13228090addef3f0ce32e246fcf0a992a7faa92f
hackarmann <anna.karmann92@gmail.com>
2016-01-13 23:05:50 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+$('.image-popup').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+		
+	});
+})
